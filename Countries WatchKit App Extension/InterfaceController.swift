da83ae85eccd11766413f16eed9a77b60b1107a5
RoderickMartin6174@gmail.com <[RoderickMartin6174@gmail.com]>
2016-08-26 11:01:29 -0700

    First commit, created country table with label.

---
+++ Countries WatchKit App Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Countries WatchKit App Extension
+//
+//  Created by Martin Zhang on 2016-08-26.
+//  Copyright © 2016 Martin Zhang. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
